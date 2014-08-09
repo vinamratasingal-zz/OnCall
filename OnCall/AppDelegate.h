0d1bcd1e84f8b6b602398a49637f2c78ebcca175
Vinamrata Singal <vinamrata@Vinamratas-MacBook-Pro.local>
2014-08-09 14:47:18 -0500

    Initial Commit

---
+++ OnCall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnCall
+//
+//  Created by Vinamrata Singal on 8/9/14.
+//  Copyright (c) 2014 Team OnCall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
