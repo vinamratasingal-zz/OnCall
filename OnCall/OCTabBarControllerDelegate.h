b4832ad030f921edb060831a19e1214bc05aaf82
Michael Fang <mjfang@stanford.edu>
2014-09-04 01:16:43 -0700

    fixed some calendar stuff + separated out tabbarcontrollerdelegate to a separate class

---
+++ OnCall/OCTabBarControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  OCTabBarControllerDelegate.h
+//  OnCall
+//
+//  Created by Michael Fang on 9/4/14.
+//  Copyright (c) 2014 Team OnCall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCTabBarControllerDelegate : NSObject
+
+@end
---
+++ OnCall/OCTabBarControllerDelegate.m
@@ -0,0 +1,13 @@
+//
+//  OCTabBarControllerDelegate.m
+//  OnCall
+//
+//  Created by Michael Fang on 9/4/14.
+//  Copyright (c) 2014 Team OnCall. All rights reserved.
+//
+
+#import "OCTabBarControllerDelegate.h"
+
+@implementation OCTabBarControllerDelegate
+
+@end
